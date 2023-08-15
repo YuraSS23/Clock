199bbed4b09bd6fe5144201bada3320b8f0ecb3c
YuraSS23 <nikitsenko_yura@mail.ru>
2023-08-15 09:49:57 +0300

    Конец рабочего дня

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
-let timerId; // переменная, которая будет хранить ID таймера
-
-let isEventListenerActive = true;
-const startButton = document.getElementById("start");
-
-startButton.addEventListener('click', clickHandler);
-
-function clickHandler() {
-    if (isEventListenerActive) {
-        // Ваш код обработки события
-        timerId = setInterval(updateClock, 1);
-        // Останавливаем выполнение обработчика события
-        startButton.removeEventListener('click', clickHandler);
-        isEventListenerActive = false;
-    }
-}
-
-const stopButton = document.getElementById("stop");
-stopButton.addEventListener('click', function () {
-    // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
-    clearInterval(timerId);// останавливаем таймер
-    isEventListenerActive = true;
-    startButton.addEventListener('click', clickHandler);
-});
-
 function updateClock() {
     const clock = document.getElementById('clock');
-    // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ new Date()
     const now = new Date();
-    const hours = now.getHours()
-    const minutes = now.getMinutes()
-    const seconds = now.getSeconds()
+    const end = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 17, 0, 0)-now;
+    const hours = Math.floor((end / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((end / (1000 * 60)) % 60);
+    const seconds = Math.floor((end / 1000) % 60);
     clock.textContent = hours + ':' + minutes + ':' + seconds;
 }
+setInterval(updateClock, 1000);
