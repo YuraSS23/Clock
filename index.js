78abf3733d5aa14bb1ee13e619d83fecd469eb91
YuraSS23 <nikitsenko_yura@mail.ru>
2023-08-15 08:39:53 +0300

    Clock last version

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 let timerId; // переменная, которая будет хранить ID таймера
 
-
+let isEventListenerActive = true;
 const startButton = document.getElementById("start");
-startButton.addEventListener('click', function () {
-    // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
-    timerId = setInterval(updateClock, 1); // запускаем  updateClock() каждую секунду
-});
 
+startButton.addEventListener('click', clickHandler);
+
+function clickHandler() {
+    if (isEventListenerActive) {
+        // Ваш код обработки события
+        timerId = setInterval(updateClock, 1);
+        // Останавливаем выполнение обработчика события
+        startButton.removeEventListener('click', clickHandler);
+        isEventListenerActive = false;
+    }
+}
 
 const stopButton = document.getElementById("stop");
 stopButton.addEventListener('click', function () {
     // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
-    clearInterval(timerId); // останавливаем таймер
+    clearInterval(timerId);// останавливаем таймер
+    isEventListenerActive = true;
+    startButton.addEventListener('click', clickHandler);
 });
-
 
 function updateClock() {
     const clock = document.getElementById('clock');
