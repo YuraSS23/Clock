103e490f8c756d953fd9c6896784a731bf094b2c
YuraSS23 <nikitsenko_yura@mail.ru>
2023-12-15 09:26:28 +0300

    Выходные+нули

--- index.js
+++ index.js
@@ -10,12 +10,24 @@ function updateClock() {
     const hours2 = Math.floor((before/ (1000 * 60 * 60)) % 24);
     const minutes2 = Math.floor((before / (1000 * 60)) % 60);
     const seconds2 = Math.floor((before / 1000) % 60);
-    if (end>30600000) {
+    if (now.getDay()===6) {
+        clock.textContent = "";
+        header.textContent = ("Ура!!! Сегодня суббота!!!");
+    } else if (now.getDay()===7) {
+        clock.textContent = "";
+        header.textContent = ("Ура!!! Сегодня воскресенье!!!");
+    } else if (end>30600000) {
         now.getDay()===5 ? header.textContent = ("Сегодня пятница!!! До начала рабочего дня"): header.textContent = "До начала рабочего дня";
-        clock.textContent = hours2 + ':' + minutes2 + ':' + seconds2;
-    } else if (end>0) {
-        now.getDay()===5 ? header.textContent = ("Сегодня пятница!!! До конца рабочего дня") : header.textContent = "До конца рабочего дня";
-        clock.textContent = hours + ':' + minutes + ':' + seconds;
+        clock.textContent =
+            (hours2 < 10 ? "0" + hours2 : hours2) + ':' +
+            (minutes2 < 10 ? "0" + minutes2 : minutes2) + ':' +
+            (seconds2 < 10 ? "0" + seconds2 : seconds2);
+    } else if (end > 0) {
+        now.getDay() === 5 ? header.textContent = ("Сегодня пятница!!! До конца рабочего дня") : header.textContent = "До конца рабочего дня";
+        clock.textContent =
+            (hours < 10 ? "0" + hours : hours) + ':' +
+            (minutes < 10 ? "0" + minutes : minutes) + ':' +
+            (seconds < 10 ? "0" + seconds : seconds);
     }
     else {
         clock.textContent = "";
